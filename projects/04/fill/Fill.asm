53bc6fc9f3e478929a077f808806d9130de23b50
sarah kim <3812995+sarahbkim@users.noreply.github.com>
2019-01-28 00:03:11 -0800

    cpu & mem

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -3,12 +3,50 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input.
+// Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+// i.e. writes "black" in every pixel. When no key is pressed, the
+// program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+@R0
+M=-1
+
+@DRAW
+0;JMP
+
+(DRAW)
+	@8191
+	D=A
+	@R1
+	M=D
+
+	(NEXT)
+		// calculate position
+		@R1
+		D=M
+		@pos
+		M=D
+		@SCREEN
+		D=A
+		@pos
+		M=M+D
+
+		// draw
+		@R0
+		D=M
+		@pos
+		A=M
+		M=D
+
+		// decrement
+		@R1
+		D=M-1
+		M=D
+
+		@NEXT
+		D;JGE
+	0;JMP
+@LOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// to solve: R2 = R1 * R0
+
+// R0 = 2, R1 = 1, R2 = 0
+
+@i
+M=1 // i = 1
+@R2
+M=0 // R2 = 0
+(LOOP)
+    @i
+    D=M // D=i
+    @R1
+    D=D-M // D = i-R1
+    @END
+    D;JGT // if (i-R1)>0 goto END
+    @R0
+    D=M // D = R0
+    @R2
+    M=D+M // R2 = R2 + R0
+    @i
+    M=M+1 // i = i + 1
+    @LOOP
+    0;JMP // goto LOOP
+(END)
+    @END
+    0;JMP
